34539444980a116b118a678ede33270518c1958d
MuhammetAlli <75321585+MuhammetAlli@users.noreply.github.com>
2023-10-03 11:33:55 +0300

    Update LevelScriptable.cs

--- LevelScriptable.cs
+++ LevelScriptable.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System;
 using System.Linq;
+using System.Text;
+using Unity.Mathematics;
 
 [CreateAssetMenu(fileName = "Level", menuName = "ScriptableObjects/Level")]
 public class LevelScriptable : SerializedScriptableObject
@@ -13,6 +15,11 @@ public class LevelScriptable : SerializedScriptableObject
     public bool editordeBuSeviyeyiKullan;
 
     public InfList<int> BossLevels;
+    public InfList<int> HardLevels;
+
+    [Range(0f, 1f)] public List<float> Yukseklikler;
+    [Range(0, 100)] public List<int> YukseklikUyumlari;
+    [Range(0, 100)] public int YukseklikUyum = 50;
 
     [Header("Satır Sayısı Eğrisi")]
     public AnimationCurve satirSayisiEgrisi;
@@ -53,7 +60,7 @@ public class LevelScriptable : SerializedScriptableObject
     public void KaydetButton()
     {
         BloklariKaydet();
-        EgrilereGoreSeviyeGuncelle();
+        // EgrilereGoreSeviyeGuncelle();
     }
 
     [OnInspectorInit]
@@ -67,69 +74,222 @@ public class LevelScriptable : SerializedScriptableObject
 
     public void SeviyeOlustur()
     {
+        #region Yoğunluk ve Tip Hesaplamaları
+        // Yoğunluk ve tipleri hesaplamak için kullanılan listelerin ve toplam yoğunluğun tanımlandığı bölüm.
         List<float> blokYogunluklari = new List<float>();
         float toplamYogunluk = 0;
 
         List<Block.BlockType> blokTipleri = new List<Block.BlockType>(blokYogunlukEgrileri.Keys);
         List<AnimationCurve> egriler = new List<AnimationCurve>(blokYogunlukEgrileri.Values);
 
+        // Yoğunlukların ve toplam yoğunluğun hesaplandığı döngü.
         for (int i = 0; i < egriler.Count; i++)
         {
-            float yogunluk = egriler[i].Evaluate((float)seviyeNumarasi / (float)BenzersizSeviyeSayisi);
+            float yogunluk = 0;
+            if (i == (int)Block.BlockType.Sandik || i == (int)Block.BlockType.Boss) yogunluk = 0f;
+            else yogunluk = egriler[i].Evaluate((float)seviyeNumarasi / (float)BenzersizSeviyeSayisi);
+            //  Debug.Log("yogunluk= " + yogunluk);
             blokYogunluklari.Add(yogunluk);
             toplamYogunluk += yogunluk;
         }
 
+        // Yoğunlukları normalleştiren döngü.
         for (int i = 0; i < blokYogunluklari.Count; i++)
         {
             blokYogunluklari[i] /= toplamYogunluk;
+            //    Debug.Log("blokYogunluklari[i]= " + blokYogunluklari[i]);
         }
 
-        int satirSayisi = Mathf.CeilToInt(satirSayisiEgrisi.Evaluate((float)seviyeNumarasi / BenzersizSeviyeSayisi));
-        BLOKLAR = new Texture2D[satirSayisi, BLOKLAR.GetLength(1)];  // Satır sayısını güncelle
+        #region Blok Adetleri
 
-        for (int i = 0; i < satirSayisi; i++)  // BLOKLAR.GetLength(0) yerine satirSayisi kullan
+        // doldurulacakBlokSayisi
+        int doldurulacakBlokSayisi;
+        doldurulacakBlokSayisi = BLOKLAR.GetLength(0) * BLOKLAR.GetLength(1);
+        doldurulacakBlokSayisi -= BLOKLAR.GetLength(0) * 1; // bir satır full tahta
+        doldurulacakBlokSayisi -= BLOKLAR.GetLength(0) * 1; // bir satır full sandık
+        if (BossLevels.IndexOf(seviyeNumarasi) != -1) doldurulacakBlokSayisi -= BLOKLAR.GetLength(0) * 1; // bir satır full boss
+
+        // blok adetleri
+        List<int> blokAdetleri = new List<int>();
+        List<float> kalanlar = new List<float>();
+        int blokAdetleriToplam = 0;
+        for (int i = 0; i < blokYogunluklari.Count; i++)
+        {
+            int adet = (int)(doldurulacakBlokSayisi * blokYogunluklari[i]);
+            kalanlar.Add(doldurulacakBlokSayisi * blokYogunluklari[i] - adet);
+            blokAdetleri.Add(adet);
+            blokAdetleriToplam += adet;
+        }
+        //   for (int i = 0; i < kalanlar.Count; i++) Debug.Log("BEFORE kalanlar " + i + " = " + kalanlar[i]);
+
+        // kalanları ekliyoruz
+        if (blokAdetleriToplam < doldurulacakBlokSayisi)
+        {
+            for (int i = 0; i < doldurulacakBlokSayisi - blokAdetleriToplam; i++)
+            {
+                int kalanlarIndex = KalanlarGetHighestIndex(kalanlar);
+                kalanlar[kalanlarIndex] -= 1;
+                blokAdetleri[kalanlarIndex]++;
+            }
+        }
+        //    for (int i = 0; i < kalanlar.Count; i++) Debug.Log("AFTER kalanlar " + i + " = " + kalanlar[i]);
+
+        //  for (int i = 0; i < blokAdetleri.Count; i++) Debug.Log("blokAdetleri " + i + " = " + blokAdetleri[i]);
+
+        // blok indexleri listesi
+        List<int> blokIndexleri = new List<int>();
+        for (int i = 0; i < blokAdetleri.Count; i++)
+            for (int j = 0; j < blokAdetleri[i]; j++)
+                blokIndexleri.Add(i);
+        #endregion
+
+        #endregion
+
+        #region Satır Sayısının ve BLOKLAR Dizisinin Güncellenmesi
+        // Satır sayısını ve BLOKLAR dizisini güncelleyen bölüm.
+
+        float levelPercent = (float)(seviyeNumarasi - 1) / (float)(BenzersizSeviyeSayisi - 1);
+        //  Debug.Log("levelPercent * satirSayisiEgrisi.keys[satirSayisiEgrisi.length - 1].time= " + levelPercent * satirSayisiEgrisi.keys[satirSayisiEgrisi.length - 1].time);
+
+
+        int satirSayisi = (int)satirSayisiEgrisi.Evaluate(levelPercent * satirSayisiEgrisi.keys[satirSayisiEgrisi.length - 1].time);
+        BLOKLAR = new Texture2D[BLOKLAR.GetLength(0), satirSayisi];  // Satır sayısını güncelle
+        #endregion
+
+        #region Blokların Doldurulması
+        // Blokları dolduran ve belirli kurallara göre belirli tipleri atayan ana döngü.
+        for (int i = 0; i < BLOKLAR.GetLength(0); i++)
         {
             for (int j = 0; j < BLOKLAR.GetLength(1); j++)
             {
-                if (i == 0)
+                #region Özel Blok Atamaları
+                // İlk satırın tamamen tahta olması
+                if (j == 0)
                 {
-                    BLOKLAR[i, j] = IndisToTexture(0); // Tahta
+                    BLOKLAR[i, j] = IndisToTexture((int)Block.BlockType.Tahta); // Tahta
                     continue;
                 }
 
-                if (i == satirSayisi - 1)
+                // Son satırın tamamen sandık olması
+                if (j == BLOKLAR.GetLength(1) - 1)
                 {
-                    BLOKLAR[i, j] = IndisToTexture(3); // Sandık
+                    BLOKLAR[i, j] = IndisToTexture((int)Block.BlockType.Sandik); // Sandık
                     continue;
                 }
 
-                if (seviyeNumarasi % 5 == 0 && i == satirSayisi - 2)
+                // Seviye numarası boss olan bir seviye ise, sondan bir önceki satırın tamamen boss olması
+                if (BossLevels.IndexOf(seviyeNumarasi) != -1 && j == BLOKLAR.GetLength(1) - 2)
                 {
-                    BLOKLAR[i, j] = IndisToTexture(4); // Boss
+                    BLOKLAR[i, j] = IndisToTexture((int)Block.BlockType.Boss); // Boss
                     continue;
                 }
+                #endregion
 
-                float rastgeleDeger = UnityEngine.Random.value;
-                float toplananYogunluk = 0;
-
-                for (int blokTipi = 0; blokTipi < blokYogunluklari.Count; blokTipi++)
+                #region Rastgele Blok Atamaları
+                //int randomIndex = UnityEngine.Random.Range(0, blokIndexleri.Count);
+                float nowYukseklik = 1f - ((float)(j - 1) / (float)(BLOKLAR.GetLength(1) - 3));
+                int randomIndex = -5;
+                int breakSayac = 0;
+                while (blokIndexleri.IndexOf(randomIndex) == -1)
                 {
-                    if (blokTipi == 0 || blokTipi == 3 || blokTipi == 4) continue;
-
-                    toplananYogunluk += blokYogunluklari[blokTipi];
-
-                    if (rastgeleDeger <= toplananYogunluk)
-                    {
-                        BLOKLAR[i, j] = IndisToTexture(blokTipi);
-                        break;
-                    }
+                    randomIndex = GetRandomBlockIndex(blokIndexleri, nowYukseklik, j == BLOKLAR.GetLength(1) - 2);
+                    breakSayac++;
+                    if (breakSayac == 1000) { Debug.Log("1000 kere denedi!"); randomIndex = blokIndexleri[0]; break; }
                 }
+
+                BLOKLAR[i, j] = IndisToTexture(randomIndex);
+                blokIndexleri.Remove(randomIndex);
+                #endregion
+
+                /* #region ESKİ Rastgele Blok Atamaları
+                   float rastgeleDeger = UnityEngine.Random.value;
+                   float toplananYogunluk = 0;
+                   bool blokAtandi = false;
+
+                   // Güncellenmiş blok atama
+                   List<float> kumulatifYogunluklar = blokYogunluklari.ToList();
+                   for (int k = 1; k < kumulatifYogunluklar.Count; k++)
+                   {
+                       kumulatifYogunluklar[k] += kumulatifYogunluklar[k - 1];
+                   }
+
+                   for (int blokTipi = 0; blokTipi < kumulatifYogunluklar.Count; blokTipi++)
+                   {
+                       // Özel durumları atla
+                       if (blokTipleri[blokTipi] == Block.BlockType.Tahta ||
+                           blokTipleri[blokTipi] == Block.BlockType.Sandik ||
+                           blokTipleri[blokTipi] == Block.BlockType.Boss) continue;
+
+                       if (rastgeleDeger <= kumulatifYogunluklar[blokTipi])
+                       {
+                           BLOKLAR[i, j] = IndisToTexture(blokTipi);
+                           blokAtandi = true;
+                           break;
+                       }
+                   }
+                 #endregion*/
             }
         }
+        #endregion
+    }
+    int KalanlarGetHighestIndex(List<float> list)
+    {
+        int index = 0; float highest = -500;
+        for (int i = 0; i < list.Count; i++) if (list[i] > highest) { highest = list[i]; index = i; }
+        return index;
     }
 
+    int GetRandomBlockIndex(List<int> blokIndexleri, float nowYukseklik, bool isDebug = false) // yüksekliğe ve kalan blok türlerine göre
+    {
+        //   Debug.Log("nowYukseklik= " + nowYukseklik);
+        List<float> diffs = new List<float>();
+        for (int i = 0; i < Yukseklikler.Count; i++)
+        {
+            if (blokIndexleri.IndexOf(i) == -1) diffs.Add(0);
+            else diffs.Add(1f - Mathf.Abs(nowYukseklik - Yukseklikler[i]));
+        }
+        //        if (isDebug) { for (int i = 0; i < diffs.Count; i++) Debug.Log("diffs " + i + " = " + diffs[i]); }
 
+        #region yukseklik uyum
+        // highestDiff
+        float highestDiff = -500; for (int i = 0; i < diffs.Count; i++) if (diffs[i] > highestDiff) { highestDiff = diffs[i]; }
+        highestDiff -= .01f;
+
+        for (int i = 0; i < diffs.Count; i++)
+        {
+            diffs[i] -= highestDiff * (YukseklikUyum * .01f);
+            //  diffs[i] -= highestDiff * (YukseklikUyumlari[i] /*YukseklikUyum*/ * .01f);
+            // diffs[i] *= (YukseklikUyumlari[i] * .01f) - (1f - diffs[i]) * 1.2f;
+            if (diffs[i] < 0) diffs[i] = 0;
+        }
+        #endregion
+
+        /*
+        
+        = .50 - (1f-.6)
+        = .50 - .4*1.5
+        = .1
+
+        = .50 - (1f-.5)
+        = .50 - .5
+        = .0
+
+        */
+
+        if (isDebug) for (int i = 0; i < diffs.Count; i++) Debug.Log("AFTER diffs " + i + " = " + diffs[i]);
+
+        float totalDiffs = 0; for (int i = 0; i < diffs.Count; i++) totalDiffs += diffs[i];
+        float randomFloat = UnityEngine.Random.Range(0f, totalDiffs);
+        float sayac = 0;
+        for (int i = 0; i < diffs.Count; i++)
+        {
+            sayac += diffs[i];
+            if (randomFloat <= sayac) return i;
+        }
+
+        Debug.Log("default RETURNLEDİ!");
+        return 0;
+    }
 
     Texture2D EnumToTexture(Block.BlockType blokTipi)
     {
@@ -339,6 +499,7 @@ public class LevelScriptable : SerializedScriptableObject
         for (int i = 1; i <= BenzersizSeviyeSayisi; i++)
         {
             seviyeNumarasi = i;
+            SeviyeNumarasiDegisti();
             SeviyeOlustur();
             BloklariKaydet();
         }
@@ -444,9 +605,59 @@ public class LevelScriptable : SerializedScriptableObject
         }
         else
         {
-            Debug.LogError("Dosya bulunamadı: " + yol);
+            // SeviyeNumarasiDegisti();
+            SeviyeOlustur();
+            BloklariKaydet();
+            Debug.Log("Dosya bulunamadı: " + yol + "  yeni oluşturuldu");
         }
     }
+
+    [ShowInInspector, ReadOnly, FoldoutGroup("Seviye Bilgileri")]
+    public string[] SeviyeBilgileriDizisi
+    {
+        get
+        {
+            // Yoğunluk ve tipleri hesaplamak
+            List<float> blokYogunluklari = new List<float>();
+            float toplamYogunluk = 0;
+
+            List<Block.BlockType> blokTipleri = new List<Block.BlockType>(blokYogunlukEgrileri.Keys);
+            List<AnimationCurve> egriler = new List<AnimationCurve>(blokYogunlukEgrileri.Values);
+
+            // Yoğunluk ve toplam yoğunluk hesaplama
+            for (int i = 0; i < egriler.Count; i++)
+            {
+                float yogunluk = egriler[i].Evaluate((float)seviyeNumarasi / (float)BenzersizSeviyeSayisi);
+                blokYogunluklari.Add(yogunluk);
+                toplamYogunluk += yogunluk;
+            }
+
+            // Yoğunlukları normalleştirme
+            for (int i = 0; i < blokYogunluklari.Count; i++)
+            {
+                blokYogunluklari[i] /= toplamYogunluk;
+            }
+
+            // Seviye bilgilerini diziye aktarma
+            string[] seviyeBilgileriDizisi = new string[blokTipleri.Count];
+            for (int i = 0; i < blokTipleri.Count; i++)
+            {
+                seviyeBilgileriDizisi[i] = $"{blokTipleri[i].ToString()}: %{blokYogunluklari[i] * 100}";
+            }
+
+            return seviyeBilgileriDizisi;
+        }
+    }
+
+
+
+
+    /*
+                seviyeBilgileri.Clear();
+                seviyeBilgileri.Append("Muhammet");
+                seviyeBilgileri.Append("Ali");
+                seviyeBilgileri.AppendLine("Yaşar");
+    */
     #endregion
 
 
